0517df07bb45ceb999649c1b9da44baafdccc24c
Dimifabbridf96 <dimi.fabbri@gmail.com>
2024-05-30 16:14:07 +0100

    added rules to element created in js and updated rules in css for better layout

--- script.js
+++ script.js
@@ -18,16 +18,14 @@ function showGames(results){
 
     results.forEach(result => {
         const gameCard = document.createElement("div");
-        gameList.classList.add("d-flex", "justify-content-around", "flex-wrap")
-        gameCard.classList.add("card", "col-lg-5", "col-sm-11");
+        gameList.classList.add("d-flex", "justify-content-around", "flex-wrap", "black")
+        gameCard.classList.add("card", "col-lg-3","col-md-6", "col-sm-11", "games");
 
         gameCard.innerHTML = `
-        <img src="${result.background_image}" class="card-img-top" alt="${result.name} card image">
+        <img src="${result.background_image}" class="card-img-top gameHeight " alt="${result.name} card image">
         <div class="card-body">
           <h5 class="card-title">${result.name}</h5>
           <p class="card-text">${result.rating}</p>
-          <div class="d-flex justify-content-center">
-            <a href="games.html" class="btn btn-primary center">Go somewhere</a>
         `;
             gameList.appendChild(gameCard);
     });
