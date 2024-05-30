d44b47bf9da7d9ce9e15d37c08611de3980351a6
Dimifabbridf96 <dimi.fabbri@gmail.com>
2024-05-30 14:11:08 +0100

    added fetch and returned the result in json format

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const url = `https://api.rawg.io/api/games?key=${apiKey}`
 document.addEventListener("load", fetchGames());
 
 function fetchGames(url){
-    
+    fetch(url)
+    .then(result => result.json());
 }
 fetchGames(url);
