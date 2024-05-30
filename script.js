c5cccb0717c594d275381448034ae4e7602572bc
Dimifabbridf96 <dimi.fabbri@gmail.com>
2024-05-30 14:06:54 +0100

    added eventListener on load with a function to start the fetching

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let gameList = document.getElementsByClassName("gameList");
 const url = `https://api.rawg.io/api/games?key=${apiKey}`
 
+document.addEventListener("load", fetchGames());
+
+function fetchGames(url){
+    
+}
+fetchGames(url);
