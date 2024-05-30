590c838380a03ed3aad6e5e2ffaf6f826ea50f45
Dimifabbridf96 <dimi.fabbri@gmail.com>
2024-05-30 15:26:51 +0100

    added results in the Dom and changed gameLIst from class to Id

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-let gameList = document.getElementsByClassName("gameList");
-const url = `https://api.rawg.io/api/games?key=${apiKey}`
+let gameList = document.getElementById("gameList");
+const url = `https://api.rawg.io/api/games?key=${apiKey}`;
 
 function fetchGames(url){
 
     fetch(url)
         .then(response => response.json())
         .then(data =>{
-            if(data.results && data.count > 0){
-                console.log(data.count);
-                console.log(data.results);
+            if(data.count > 0){
                 showGames(data.results);
             }
 
         }) 
+}
+
+function showGames(results){
+    gameList.innerHTML = "";
+
+    results.forEach(result => {
+        const gameCard = document.createElement("div");
+        gameCard.classList.add("card", "col-lg-5", "col-sm-11");
+
+        gameCard.innerHTML = `
+        <img src="${result.background_image}" class="card-img-top" alt="${result.name} card image">
+        <div class="card-body">
+          <h5 class="card-title">${result.name}</h5>
+          <p class="card-text">${result.rating}</p>
+          <div class="d-flex justify-content-center">
+            <a href="games.html" class="btn btn-primary center">Go somewhere</a>
+        `;
+            gameList.appendChild(gameCard);
+    });
 }
 fetchGames(url);
